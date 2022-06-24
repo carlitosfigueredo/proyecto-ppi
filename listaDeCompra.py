4abf4ba2762fdece2c65cd9a88274dd3c6ed2b5b
Carlos Figueredo <carlos@nosotroshq.com>
2022-06-24 10:35:39 -0400

    Motivo: Update del Proyecto
    [Autor: Carlos Figueredo AKA: Dramitas]

---
+++ listaDeCompra.py
@@ -0,0 +1,3 @@
+listaCompra = '[{"producto":"JUGO DE NARANJA FRUGOS","cantidad":"2"},{"producto": "DESODORANTE DOVE MEN","cantidad": "2"},{"producto": "TE ROJO","cantidad": "1"},{"producto":"CARNE MOLIDA POPULAR","cantidad": "2"},{"producto": "MILANESA DE POLLO","cantidad": "10"},{"producto":"SALSA DE TOMATE","cantidad": "10"},{"producto":"VINO SANTA HELENA","cantidad": "1"},{"producto":"MANTECA","cantidad": "1"},{"producto":"YERBA MATE","cantidad": "2"},{"producto":"CAFE TORRADO","cantidad": "1"}]'
+
+
