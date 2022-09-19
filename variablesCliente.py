2386032ac9bbd9e165eb11eea850dc585c1f6045
carlitosfigueredo <73946203+carlitosfigueredo@users.noreply.github.com>
2022-09-18 22:00:43 -0400

    Cambio de pass

--- variablesCliente.py
+++ variablesCliente.py
@@ -1,5 +1,5 @@
-usuarioSuper6 = "carlosalberto.figueredoquevedo@gmail.com"
-passwordSuper6 = "HolaManola$$2020"
+usuarioSuper6 = "email"
+passwordSuper6 = "password"
 chatIdTelegramCliente = "640626424"
 nombreCliente = 'Carlos Figueredo'
 mensajeCarrito = 'Hola '+nombreCliente+'.\nSe ha terminado el proceso de llenado de su carrito de compras del Super6 Online\nFavor inicie sesion en su dispositivo preferido para terminar de pagar su compra.\nMuchas gracias por utilizarme!!'
